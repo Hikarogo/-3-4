edbeb85ab33612fe102bdc3a19a24bc5bc3353ce
Студент 3 < 3@DESKTOP-PS97MJ6>
2022-10-24 12:13:20 +0300

    Добавьте файлы проекта.

---
+++ задачи/Program.cs
@@ -0,0 +1,30 @@
+﻿////int a = 5, b = 13;
+////for (int i = a; i <= b; i++)
+////{
+////    for (int j = 1; j <= i; j++)
+////    {
+////        Console.Write($" {i} ");
+
+////    }
+////    Console.WriteLine();
+//}
+Console.WriteLine("ваше число a");
+String a =Console.ReadLine();
+int f = int.Parse(a);
+Console.WriteLine("ваше число b");
+string b = Console.ReadLine();
+int g = int.Parse(b);
+Console.WriteLine();
+if (f < g)
+{
+    for (int i = f; i <= g; i++)
+    {
+        Console.WriteLine(i);
+     
+    }
+}
+else
+{
+    for(int j = f; j >= g; j--)
+        Console.WriteLine(j);  
+}
